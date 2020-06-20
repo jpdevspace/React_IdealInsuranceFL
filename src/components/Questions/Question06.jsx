5a4f81ba72bf9a629dd89011826b0a0a19fb2ccc
Jean Paul <jjpwebdev@gmail.com>
2020-06-19 23:14:42 -0400

    All fields added to question 6

--- src/components/Questions/Question06.jsx
+++ src/components/Questions/Question06.jsx
@@ -1,40 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import LanguageContext from "../../Context/LanguageContext";
 
 const Question06 = ({ updateAnswer, currAnswer }) => {
   const [language] = useContext(LanguageContext);
+  const [selected, setSelected] = useState("");
+  
+  useEffect(() => {
+    updateAnswer(selected, true);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selected])
 
   const questions = {
     english: {
       title: "Contact Information",
       name: "Name",
       dob: "Date of Birth",
-      month: "Month",
-      day: "Day",
-      year: "Year",
       gender: "Gender",
       phone: "Phone Number"
     },
     spanish: {
       title: "Información de Contacto",
       name: "Nombre",
-      dob: "Fecha de Nacimiento",
-      month: "Mes",
-      day: "Día",
       year: "Año",
       gender: "Genero",
       phone: "Número de Teléfono"
     }
   }
 
-  const handleChange = (e) => {
-    const val = e.target.value;
+  const handleChange = e => {
+    const { value, name } = e.target;
 
-    updateAnswer(val, val.length > 0);
+    setSelected({
+      ...selected,
+      [name]: value
+    });
   };
 
   const q = questions[language];
-  console.log("Q >", q)
+
   return (
     <>
       <h3>{q.title}</h3>
@@ -43,10 +46,51 @@ const Question06 = ({ updateAnswer, currAnswer }) => {
         <input
           onChange={e => handleChange(e)}
           value={ currAnswer ? currAnswer.name : "" }
+          name="name"
           placeholder={q.name}
           required 
           type="text"
         />
+      </label>
+      <label>
+        { q.phone }
+        <input
+          onChange={e => handleChange(e)}
+          value={ currAnswer ? currAnswer.phone : "" }
+          name="phone"
+          placeholder={`${q.phone} e.g. 123-456-7890`}
+          required 
+          type="tel"
+        />
+      <small>Format: 123-456-7890</small>
+      </label>
+      <label>
+        { q.dob }
+        <input
+          onChange={e => handleChange(e)}
+          value={ currAnswer ? currAnswer.dob : "" }
+          name="dob"
+          placeholder={q.dob}
+          required 
+          type="date"
+        />
+      </label>
+      <label>
+        { q.gender }
+        <select
+          name="gender"
+          value={currAnswer ? currAnswer.gender : "female"}
+          onChange={e => handleChange(e)}>
+          <option 
+            value="female">
+              {language === "english" ? "Female" : "Femenino"}
+          </option>
+          <option
+            name="gender"
+            value="male">
+              {language === "english" ? "Male" : "Masculino"}
+          </option>
+        </select>
       </label>
     </>
   );
